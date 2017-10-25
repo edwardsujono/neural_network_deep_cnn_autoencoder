48a15c452944fbeace856b37d4cf2aa34d83fb17
AssignmentNTU <edwardsujono81@gmail.com>
2017-10-26 00:48:53 +0800

    refactor the code, since there is a misunderstanding regarding with the encoder and decoer architecture

--- project_b/autoencoder_softmax.py
+++ project_b/autoencoder_softmax.py
@@ -1,13 +1,12 @@
 import numpy as np
-from common.utils import init_bias,init_weights
+from common.utils import init_bias, init_weights
 import theano
 import theano.tensor as T
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 
 class SoftmaxAutoEncoder:
-
-    def __init__(self, num_features, num_outputs, list_hidden_layer,learning_rate,
+    def __init__(self, num_features, num_outputs, list_hidden_layer, learning_rate,
                  corruption_level=0.01,
                  sparsity_parameter=0.01, penalty_parameter=0.01):
 
@@ -19,7 +18,7 @@ class SoftmaxAutoEncoder:
         :param penalty_parameter: learning decay
         """
 
-        list_neurons = [num_features] + list_hidden_layer + [num_outputs]
+        list_neurons = [num_features] + list_hidden_layer
         x = T.matrix('x')
         d = T.matrix('d')
 
@@ -33,63 +32,72 @@ class SoftmaxAutoEncoder:
             Construct the auto encoder
         """
 
-        weight_ae = init_weights(list_neurons[0], list_neurons[1], name_weight="weight_auto_encoder")
-        bias_ae = init_bias(list_neurons[1], name_bias="bias_ae")
+        weights = []
+        biases = []
 
-        bias_ae_pm = init_bias(list_neurons[0], name_bias="bias_ae_pm")
+        prev_output = x
 
-        y = T.nnet.sigmoid(T.dot(x, weight_ae) + bias_ae)
-        z = T.nnet.sigmoid(T.dot(y, T.transpose(weight_ae)) + bias_ae_pm)
+        """
+            ENCODER
+        """
 
-        cost = - T.mean(T.sum(x * T.log(z) + (1 - x) * T.log(1 - z), axis=1)) - \
-                    penalty_parameter * (sparsity_parameter * T.log(sparsity_parameter)
-                                         + (1 - sparsity_parameter) * T.log(1 - sparsity_parameter))\
-               + penalty_parameter * sparsity_parameter * T.sum(T.log(T.mean(y, axis=0) + 1e-6))\
-               + penalty_parameter * (1 - sparsity_parameter) * T.sum(T.log(1 - T.mean(y, axis=0) + 1e-6))
+        for i in range(1, len(list_neurons)):
 
-        params = [weight_ae, bias_ae, bias_ae_pm]
+            weight = init_weights(list_neurons[i-1], list_neurons[i], 'weight_%s' % i)
+            bias = init_bias(list_neurons[i], 'bias_%s' % i)
 
+            weights.append(weight)
+            biases.append(bias)
+
+            prev_output = T.nnet.sigmoid(T.dot(prev_output, weight) + bias)
+
+        """
+            DECODER
+        """
+
+        buffer_output = prev_output
+        biases_trans = []
+
+        for i in range(len(weights)-1, -1, -1):
+
+            weight_transpose = weights[i].transpose()
+            bias_transpose = init_bias(list_neurons[i], 'bias_trans_%s' % i)
+
+            biases_trans.append(bias_transpose)
+
+            prev_output = T.nnet.sigmoid(T.dot(prev_output, weight_transpose) + bias_transpose)
+
+        cost = - T.mean(T.sum(x * T.log(prev_output) + (1 - x) * T.log(1 - prev_output), axis=1))
+
+        params = weights+biases+biases_trans
         grads = T.grad(cost, params)
-
         updates = [(param, param - learning_rate*grad) for param, grad in zip(params, grads)]
 
         self.train_encoder = theano.function(
             inputs=[x],
             updates=updates,
-            outputs=[z, cost]
+            outputs=[prev_output, cost]
         )
 
         """
-            Construct the full connected neural network
+            TRAIN THE FULL CONNECTED LAYER
         """
 
-        prev_output = y
-        list_weight = [weight_ae]
-        list_bias = [bias_ae]
+        last_weight = init_weights(list_neurons[-1], num_outputs, 'last_weight')
+        last_bias = init_bias(num_outputs, 'last_bias')
 
-        for i in range(2, len(list_neurons)):
+        buffer_output = T.nnet.softmax(T.dot(buffer_output, last_weight) + last_bias)
+        y_pred = T.argmax(buffer_output, axis=1)
 
-            weight = init_weights(list_neurons[i-1], list_neurons[i], 'weights_'+str(i))
-            bias = init_bias(list_neurons[i], 'bias_'+str(i))
+        cost_cross = T.mean(T.nnet.categorical_crossentropy(buffer_output, d))
 
-            if i < len(list_neurons)-1:
-                prev_output = T.nnet.sigmoid(T.dot(prev_output, weight) + bias)
-            else:
-                # last layer need to do classification
-                prev_output = T.nnet.softmax(T.dot(prev_output, weight) + bias)
-
-            list_weight.append(weight)
-            list_bias.append(bias)
-
-        y_pred = T.argmax(prev_output, axis=1)
-        cost_cross = T.mean(T.nnet.categorical_crossentropy(prev_output, d))
-        params_cross = list_weight+list_bias
-        grads_cross = T.grad(cost_cross, params_cross)
-        updates_cross = [(param, param - learning_rate*grad) for param, grad in zip(params_cross, grads_cross)]
+        params_full = weights + [last_weight] + biases + [last_bias]
+        grads_full = T.grad(cost_cross, params_full)
+        updates_full = [(param, param - learning_rate * grad) for param, grad in zip(params_full, grads_full)]
 
         self.train_cross = theano.function(
             inputs=[x, d],
-            updates=updates_cross,
+            updates=updates_full,
             outputs=[prev_output, y_pred, cost_cross]
         )
 
--- project_b/start.py
+++ project_b/start.py
@@ -1,4 +1,4 @@
-from softmax_with_autoencoder import SoftmaxAutoEncoder
+from autoencoder_softmax import SoftmaxAutoEncoder
 from common.data_collector import DataCollector
 
 if __name__ == "__main__":
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     softmax = SoftmaxAutoEncoder(num_features=num_feature, num_outputs=10,
                                  list_hidden_layer=[900], learning_rate=0.05)
 
-    # softmax.start_train_auto_encoder(epochs=10, batch_size=20000,
-    #                                  train_x=train_x, train_y=train_y)
+    softmax.start_train_auto_encoder(epochs=10, batch_size=20000,
+                                     train_x=train_x, train_y=train_y)
 
     softmax.start_train_the_full(epochs=100, batch_size=20000,
                                  train_x=train_x, train_y=train_y,
